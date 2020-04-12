e6d44f8b87792f82f4a1776886ce67b6cf06d3bf
Jong-Hyouk Yun <ageldama@gmail.com>
2020-04-12 20:55:38 +0900

    --wip-- [skip ci]

--- src/main/kotlin/jhyun/springjdbctxhello/SpringJdbcTxHelloApplication.kt
+++ src/main/kotlin/jhyun/springjdbctxhello/SpringJdbcTxHelloApplication.kt
@@ -11,6 +11,7 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.TransactionDefinition
+import org.springframework.transaction.TransactionException
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.support.TransactionTemplate
@@ -55,8 +56,9 @@ open class PostService(
     fun doSome() {
         postRepository.deleteById(2)
         //
-        val transactionTemplate = TransactionTemplate(transactionManager);
-        transactionTemplate.propagationBehavior = TransactionDefinition.PROPAGATION_REQUIRES_NEW
+        val transactionTemplate = TransactionTemplate(transactionManager).apply {
+            propagationBehavior = TransactionDefinition.PROPAGATION_REQUIRES_NEW
+        }
         transactionTemplate.execute {
             try {
                 postRepository.deleteById(3)
